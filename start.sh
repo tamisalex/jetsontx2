129ae20147f840be0bdb33c46f0ef4de30a49701
Alex Tam <tamisalex@gmail.com>
2018-10-25 20:37:05 +0000

    add -y

--- start.sh
+++ start.sh
@@ -1,20 +1,20 @@
 #install tensorflow & object_detection 
 sudo apt install -f install
 sudo apt update
-sudo apt install python-dev python-setuptools
-sudo easy_install pip
+sudo apt install -y python-dev python-setuptools
+sudo easy_install -y pip
 sudo pip install tensorflow-1.9.0rc0-cp27-cp27mu-linux_aarch64.whl
 mkdir libs
 cd libs
 git clone https://github.com/tensorflow/models
 cd ..
-sudo apt install protobuf-compiler python-pil python-lxml python-tk
+sudo apt install -y protobuf-compiler python-pil python-lxml python-tk
 pip install --user Cython
 pip install --user contextlib2
 sudo pip install --user pyzmq==17.0.0
 pip install --user jupyter
-sudo apt install libfreetype6-dev 
-sudo apt install pkg-config
+sudo apt install -y libfreetype6-dev 
+sudo apt install -y pkg-config
 pip install --user matplotlib
 cd libs
 git clone https://github.com/cocodataset/cocoapi.git
@@ -33,17 +33,17 @@ cd ~/libs
 git clone https://github.com/jetsonhacks/installROSTX2
 ./installROSTX2/installROS.sh
 ./installROSTX2/setupCatkinWorkspace.sh
-sudo apt install ros-kinetic-image-transport
-sudo apt install ros-kinetic-cv-bridge
-sudo apt install ros-kinetic-image-geometry
+sudo apt install -y ros-kinetic-image-transport
+sudo apt install -y ros-kinetic-cv-bridge
+sudo apt install -y ros-kinetic-image-geometry
 #install ptgrey camera drivers
-sudo apt-get install libraw1394-11 libavcodec-ffmpeg56 \
+sudo apt-get install -y libraw1394-11 libavcodec-ffmpeg56 \
         libavformat-ffmpeg56 libswscale-ffmpeg3 libswresample-ffmpeg1 \
         libavutil-ffmpeg54 libgtkmm-2.4-dev libglademm-2.4-dev \
         libgtkglextmm-x11-1.2-dev libusb-1.0-0
 tar xvf ~/flycapture.2.12.3.31_arm64.tar.gz -C ~/libs
 sudo sh ~/libs/flycapture.2.12.3.31_arm64/flycap2-conf
-sudo apt install ros-kinetic-pointgrey-camera-driver
+sudo apt install -y ros-kinetic-pointgrey-camera-driver
 cd ..
 sudo jetson_clocks.sh
 sudo sysctl -w net.core.rmem_max=33554432 net.core.rmem_default=33554432
