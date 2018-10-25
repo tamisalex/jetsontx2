4e949d7ce4ae3d97e6b1574b46b36c018c6da8b5
Alex Tam <tamisalex@gmail.com>
2018-10-25 20:54:10 +0000

    pwd updates

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #install tensorflow & object_detection 
+pwd=$(pwd)
 sudo apt install -f install
 sudo apt update
 sudo apt install -y python-dev python-setuptools
@@ -21,7 +22,7 @@ git clone https://github.com/cocodataset/cocoapi.git
 cd cocoapi/PythonAPI
 make
 cp -r pycocotools ~/libs/models/research/
-cd ~/libs/models/research
+cd ${pwd}/libs/models/research
 wget -O protobuf.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.6.1/protoc-3.6.1-linux-aarch_64.zip
 unzip protobuf.zip
 ./bin/protoc object_detection/protos/*.proto --python_out=.
@@ -29,7 +30,7 @@ echo 'export PYTHONPATH=/home/nvidia/catkin_ws/src/models/research${PYTHONPATH:+
 echo 'export PYTHONPATH=/home/nvidia/caktin_ws/src/models/research/slim${PYTHONPATH:+${PYTHONPATH}}'
 # check installation of tensorflow
 # install ROS
-cd ~/libs
+cd ${pwd}/libs
 git clone https://github.com/jetsonhacks/installROSTX2
 ./installROSTX2/installROS.sh
 ./installROSTX2/setupCatkinWorkspace.sh
@@ -42,9 +43,9 @@ sudo apt-get install -y libraw1394-11 libavcodec-ffmpeg56 \
         libavutil-ffmpeg54 libgtkmm-2.4-dev libglademm-2.4-dev \
         libgtkglextmm-x11-1.2-dev libusb-1.0-0
 tar xvf ~/flycapture.2.12.3.31_arm64.tar.gz -C ~/libs
-sudo sh ~/libs/flycapture.2.12.3.31_arm64/flycap2-conf
+sudo sh ./libs/flycapture.2.12.3.31_arm64/flycap2-conf
 sudo apt install -y ros-kinetic-pointgrey-camera-driver
-cd ..
+cd ~
 sudo jetson_clocks.sh
 sudo sysctl -w net.core.rmem_max=33554432 net.core.rmem_default=33554432
 sudo sysctl -w net.core.wmem_max=33554432 net.core.wmem_default=33554432
